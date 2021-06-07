44f2bf15efdbdd6f0063ae06a48c83b9a8ce5c0b
Emmapr123 <emmapriester@hotmail.com>
2021-06-07 23:43:00 +0100

    Styled Edit Exercise Component

--- src/components/Edit-exercise-component/Edit-exercise-component.tsx
+++ src/components/Edit-exercise-component/Edit-exercise-component.tsx
@@ -1,11 +1,7 @@
-import { useNavigation } from '@react-navigation/core';
-import React, {useState} from 'react';
+import React from 'react';
 import { 
   StyleSheet, 
-  Text, 
-  SafeAreaView, 
   View,
-  Dimensions,
   TextInput
 } from 'react-native';
 import { ExerciseComponentProps } from '../Exercise-component'
@@ -17,11 +13,44 @@ const EditExerciseComponent = ({
   description,
   onChange
 }:ExerciseComponentProps) => {
-  return <View>
-    <TextInput placeholder={'Add title'} value={title} onChangeText={(text) => onChange('title', text)}/>
-    <TextInput value={`${duration}`} onChangeText={(text) => onChange('duration', text)}/>
-    <TextInput placeholder={'Add description'} value={description} onChangeText={(text) => onChange('description', text)} />
+  return <View style={styles.editExerciseBox}>
+    <View>
+    <TextInput style={styles.title} placeholder={'Add title'} value={title} onChangeText={(text) => onChange('title', text)}/>
+    <TextInput style={styles.description} placeholder={'Add description'} value={description} onChangeText={(text) => onChange('description', text)} />
+    </View>
+    <View style={styles.line}/>
+    <TextInput style={styles.duration} value={`${duration}`} onChangeText={(text) => onChange('duration', text)}/>
   </View>
 }
+
+const styles = StyleSheet.create({
+  editExerciseBox: {
+    borderWidth: 0.5,
+    borderColor: 'black',
+    borderRadius: 4,
+    marginHorizontal: 10,
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    height: 150
+  },
+  title: {
+    fontSize: 18,
+    marginBottom: 50
+  },
+  duration: {
+    color: 'gray',
+    fontSize: 18
+
+  },
+  description: {
+    fontSize: 18
+  },
+  line: {
+    borderRightColor: 'black',
+    borderRightWidth: 0.2,
+    marginVertical: 5
+  }
+})
 
 export { EditExerciseComponent } 
