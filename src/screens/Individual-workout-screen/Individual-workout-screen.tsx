b174f6ba0929a7e1e6c806e2432f4ff49f35fcc9
Emmapr123 <emmapriester@hotmail.com>
2021-06-08 16:36:48 +0100

    Started styling the individual workout screen

--- src/screens/Individual-workout-screen/Individual-workout-screen.tsx
+++ src/screens/Individual-workout-screen/Individual-workout-screen.tsx
@@ -1,32 +1,33 @@
 import { useNavigation, useRoute } from '@react-navigation/core';
-import React from 'react';
+import React, { useState } from 'react';
 import { 
   View,
   Text,
+  ScrollView,
 } from 'react-native';
-import { Workout, useMyContext, Exercise, ExerciseComponent } from '../../components';
+import { Workout, ExerciseComponent, Button, Exercise } from '../../components';
 
 const IndividualWorkoutScreen = ( {workout}: {workout: Workout} ) => {
   
   const route = useRoute()
   const fullWorkout = route?.params?.workout
-  const title = fullWorkout?.title
-  console.log('-------title', title)
-  const myContext = useMyContext()
+  const workoutTitle = fullWorkout?.title
   const exercises = fullWorkout?.exercises
-  console.log('-------ex.', exercises)
-
+  const [currentIndex,setCurrentIndex] = useState(0)
+  const [title,setTitle] = useState('')
 
   return(
-    <View>
-      <Text>{title}</Text>
-      {exercises?.map((exercise : Exercise) => {
-        return <View key={exercise.description}>
-           <Text>{exercise.title}</Text>
-           <Text>{exercise.description}</Text>
-           <Text>{exercise.duration}</Text>
-           </View>
-      })}
+    <View style={{flex: 1, position: 'relative'}}>
+      <Text>{workoutTitle}</Text>
+      <ScrollView>
+      {exercises.map((exercise: Exercise,index: string) =>  {
+          return <View key={index} >
+            <ExerciseComponent  title={exercise.title} duration={exercise.duration} description={exercise.description} onChange={() => console.log('changed')} />
+          </View>
+    })}
+      </ScrollView>
+      <Button text={'Start'} onPress={() => console.log('start')} />
+      <Button text={'Edit'} onPress={() => console.log('pressed')}/>
     </View>
   )
 }
