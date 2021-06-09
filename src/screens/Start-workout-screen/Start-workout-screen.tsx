7782424a5c4036724da12dfb5ef861365f886339
Emmapr123 <emmapriester@hotmail.com>
2021-06-09 10:21:33 +0100

    Created Flatlist with exercises to scroll through

--- src/screens/Start-workout-screen/Start-workout-screen.tsx
+++ src/screens/Start-workout-screen/Start-workout-screen.tsx
@@ -1,13 +1,49 @@
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/core';
 import React from 'react';
 import { 
   Text,
   View,
+  Animated,
+  StyleSheet,
+  Dimensions
 } from 'react-native';
+import { RootStackParamList } from '../../../App';
+import { Workout } from '../../components';
 
-const StartWorkoutScreen = () => {
+type StartWorkoutScreenProp = RouteProp<
+  RootStackParamList,
+  'StartWorkoutScreen'
+>
+
+const StartWorkoutScreen = ( {workout}: {workout: Workout}) => {
+
+  const route = useRoute<StartWorkoutScreenProp>();
+  const id = route?.params?.workout.id
+  const workoutTitle = route?.params?.workout.title
+  const exercises = route?.params?.workout.exercises
+  const { width, height } = Dimensions.get('window')
+
   return(
-    <View>
-      <Text>Hello World</Text>
+    <View style={{flex: 1}}>
+      <Text>{workoutTitle}</Text>
+      <Animated.FlatList 
+        data={exercises}
+        keyExtractor={exercises => exercises.title}
+        horizontal
+        bounces={false}
+        style={[StyleSheet.absoluteFillObject, {
+          height,
+          width,
+        }]}
+        snapToInterval={width}
+        // ref={(ref) => { FlatListRef = ref; }}
+        renderItem={({item, index}) => {
+          return <View style={{width}}>
+              <Text>{item.title}</Text>
+              <Text>{item.duration}</Text>
+            </View>
+        }}
+      />
     </View>
   )
 }
