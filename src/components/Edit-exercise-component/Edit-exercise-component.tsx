bc80ef5ed74dc5867c3ee6016982c6dcf2343fe2
Emmapr123 <emmapriester@hotmail.com>
2021-06-09 11:11:05 +0100

    Set duration with minute and second picker

--- src/components/Edit-exercise-component/Edit-exercise-component.tsx
+++ src/components/Edit-exercise-component/Edit-exercise-component.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {Picker} from '@react-native-picker/picker';
 import { 
   StyleSheet, 
   View,
@@ -13,13 +14,45 @@ const EditExerciseComponent = ({
   description,
   onChange
 }:ExerciseComponentProps) => {
+
+  const [seconds, setSeconds] = useState(0)
+  const [minutes, setMinutes] = useState(0)
+
+  const secondsArray = [0,1,2,3,4,5,6,7,8,9,10,15,20,25,30,35,40,45,50,55];
+  const minutesArray = [0,1,2,3,4,5,6,7,8,9,10,15,20,25,30,35,40,45,50,55,60];
+
+  useEffect(() => {
+    onChange('duration', `${(minutes * 60 + seconds)}`)
+  }, [minutes, seconds])
+
+
   return <View style={styles.editExerciseBox}>
     <View>
     <TextInput style={styles.title} placeholder={'Add title'} value={title} onChangeText={(text) => onChange('title', text)}/>
     <TextInput style={styles.description} placeholder={'Add description'} value={description} onChangeText={(text) => onChange('description', text)} />
     </View>
     <View style={styles.line}/>
-    <TextInput style={styles.duration} value={`${duration}`} onChangeText={(text) => onChange('duration', text)}/>
+    <Picker
+        style={{flex: 1}}
+        selectedValue={minutes}
+        onValueChange={(itemValue) =>
+          setMinutes(itemValue)
+        }>
+            {minutesArray.map((minute) => {
+             return <Picker.Item key={minute} label={`${minute}`} value={minute} />
+            })}
+      </Picker>
+      <Picker
+        style={{flex: 1}}
+        selectedValue={seconds}
+        onValueChange={(itemValue) => 
+          setSeconds(itemValue)
+        }>
+          {secondsArray.map((seconds) => {
+             return <Picker.Item key={seconds} label={`${seconds}`} value={seconds} />
+            })}
+      </Picker>
+    {/* <TextInput style={styles.duration} value={`${duration}`} onChangeText={(text) => onChange('duration', text)}/> */}
   </View>
 }
 
