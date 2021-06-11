7c6c7b66dc9138748cc4373b30a0b265e0bd35cc
Emmapr123 <emmapriester@hotmail.com>
2021-06-11 14:49:29 +0100

    Refactored Start Workout screen down to 55 lines

---
+++ src/components/Start-workout-component/Start-workout-component.tsx
@@ -0,0 +1,47 @@
+import { RouteProp } from '@react-navigation/core'
+import React from 'react'
+import { 
+  View,
+  Text,
+  StyleSheet,
+  Dimensions
+ } from "react-native"
+import { RootStackParamList } from '../../../App'
+import { AnimatedBarComponent } from "../Animated-countdown-bar-component"
+import { Exercise } from '../Context-provider'
+
+export type StartWorkoutComponentProp = RouteProp<
+  RootStackParamList,
+  'StartWorkoutScreen'
+>
+
+const StartWorkoutComponent = ( {onAnimationComplete, currentIndex, index, item}:  {onAnimationComplete: () => void, currentIndex: number, index: number, item: Exercise}) => {
+ 
+  const { width } = Dimensions.get('window')
+
+  return (
+    <View key={index} style={{width}}>
+      <Text style={styles.title} >{item.title}</Text>
+      <Text style={styles.description} > {item.description} </Text>
+      <AnimatedBarComponent exercise={item} active={index === currentIndex} {...{onAnimationComplete}}/>
+    </View>
+  )}
+
+  const styles = StyleSheet.create({
+    title: {
+      fontSize: 36,
+      justifyContent: 'center',
+      alignSelf: 'center',
+      marginTop: 30,
+      fontFamily: 'AppleSDGothicNeo-Regular',
+    },
+    description: {
+      fontFamily: 'AppleSDGothicNeo-Regular',
+      fontSize: 18,
+      justifyContent: 'center',
+      alignSelf: 'center',
+      marginTop: 10,
+    }
+  })
+
+export { StartWorkoutComponent }
--- src/screens/Start-workout-screen/Start-workout-screen.tsx
+++ src/screens/Start-workout-screen/Start-workout-screen.tsx
@@ -1,26 +1,25 @@
 import { useNavigation, useRoute } from '@react-navigation/core';
 import React, { useRef, useState } from 'react';
 import { 
-  Text,
   View,
   StyleSheet,
   Dimensions,
   FlatList,
 } from 'react-native';
-import { Workout, AnimatedBarComponent } from '../../components';
+import { Workout, StartWorkoutComponent } from '../../components';
 import { StartWorkoutScreenProp } from './start-workout-screen.types';
 
 const StartWorkoutScreen = ( {workout}: {workout: Workout}) => {
 
   const route = useRoute<StartWorkoutScreenProp>();
   const navigation = useNavigation()
-  const workoutTitle = route?.params?.workout.title
   const exercises = route?.params?.workout.exercises
   const { width, height } = Dimensions.get('window')
   const [currentIndex, setCurrentIndex] = useState(0)
   let flatListRef = useRef<FlatList>(null);
 
   const onAnimationComplete = () => {
+
     if (currentIndex < exercises.length - 1) {
     flatListRef.current?.scrollToIndex({animated: true, index: currentIndex + 1})
     } else {
@@ -46,32 +45,11 @@ const StartWorkoutScreen = ( {workout}: {workout: Workout}) => {
         }]}
         snapToInterval={width}
         renderItem={({item, index}) => {
-          return <View key={index} style={{width}}>
-              <Text style={styles.title} >{item.title}</Text>
-              <Text style={styles.description} > {item.description} </Text>
-              <AnimatedBarComponent exercise={item} active={index === currentIndex} {...{onAnimationComplete}}/>
-            </View>
+          return <StartWorkoutComponent {...{onAnimationComplete, index, currentIndex, item}} />
         }}
       />
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  title: {
-    fontSize: 36,
-    justifyContent: 'center',
-    alignSelf: 'center',
-    marginTop: 30,
-    fontFamily: 'AppleSDGothicNeo-Regular',
-  },
-  description: {
-    fontFamily: 'AppleSDGothicNeo-Regular',
-    fontSize: 18,
-    justifyContent: 'center',
-    alignSelf: 'center',
-    marginTop: 10,
-  }
-})
 
 export { StartWorkoutScreen } 
